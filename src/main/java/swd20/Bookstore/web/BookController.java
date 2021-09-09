0f735c0b9bde5bcacaa19268b0bf28e9434b4cdb
Tiia Haverinen <tiiahaverinen@Tiia-MBP.home>
2021-09-09 17:34:23 +0300

    ekat lisäiykset

---
+++ src/main/java/swd20/Bookstore/web/BookController.java
@@ -0,0 +1,16 @@
+package swd20.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BookController {
+	
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String getCars(Model model) {
+		return "Moi";
+	}
+
+}
